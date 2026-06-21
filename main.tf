b90d3aeefc3c80dc7c5eff753f4f963283c1502a
rajsharmabkbiet25-stack <raj.sharmabkbiet25@gmail.com>
2026-06-21 13:55:01 +0530

    add user block inside main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,26 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
     values = ["al2023-ami-*-x86_64"]
+  }
+
+  owners = ["137112412989"] # Amazon
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.micro"
+
+  user_data = <<-EOF
+#!/bin/bash
+dnf install -y java-17-amazon-corretto tomcat
+systemctl enable tomcat
+systemctl start tomcat
+EOF
+
+  tags = {
+    Name = "HelloWorld"
   }
 }
