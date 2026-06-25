bf733ab696d55732abe7e3169aafd4ead6534c21
rajsharmabkbiet25-stack <raj.sharmabkbiet25@gmail.com>
2026-06-25 11:33:51 +0530

    fix typo

--- main.tf
+++ main.tf
@@ -24,19 +24,13 @@ systemctl enable tomcat
 systemctl start tomcat
 EOF
 
-<<<<<<< HEAD
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-=======
-  vpc_security_group_ids = [aws_security_group.blog.id]
-
->>>>>>> a98cbd6c169ee56ee77346505733422ebf827346
   tags = {
     Name = "HelloWorld"
   }
 }
 
-<<<<<<< HEAD
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "6.0.0"
@@ -88,41 +82,3 @@ resource "aws_security_group_rule" "blog_everything_out" {
 
   security_group_id = aws_security_group.blog.id
 }
-=======
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
->>>>>>> a98cbd6c169ee56ee77346505733422ebf827346
