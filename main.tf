1452223d363ff6e24efefa79de7c536d20d0b933
rajsharmabkbiet25-stack <raj.sharmabkbiet25@gmail.com>
2026-06-25 14:50:27 +0530

    create module for vpc

--- main.tf
+++ main.tf
@@ -9,13 +9,29 @@ data "aws_ami" "app_ami" {
   owners = ["137112412989"] # Amazon
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   user_data = <<-EOF
 #!/bin/bash
@@ -36,7 +52,7 @@ module "blog_sg" {
   version = "6.0.0"
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   # ingress_rules = ["http-80-tcp" , "https-443-tcp"]
   # ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -75,7 +91,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id =  module.blog_vpc.vpc_id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
