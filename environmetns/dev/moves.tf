35fba41b93a9a9df25814825e2e3596e0e0b5ee6
rajsharmabkbiet25-stack <raj.sharmabkbiet25@gmail.com>
2026-06-26 12:46:50 +0530

    Deploy deb environments

---
+++ environmetns/dev/moves.tf
@@ -0,0 +1,26 @@
+moved {
+    from = module.blog_vpc
+    to = module.dev.module.blog_vpc
+}
+
+moved {
+    from = module.blog_sg
+    to = module.dev.module.blog_sg
+}
+
+moved {
+    from = module.bweb_alb
+    to = module.dev.module.web_alb
+}
+
+moved {
+    from = module.blog_asg
+    to = module.dev.module.blog_asg
+}
+
+moved {
+    from = aws_lb_target_group.blog
+    to = module.dev.aws_lb_target_group.blog
+}
+
+  
