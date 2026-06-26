9866e64e8a262e0e3b7e0519eac50433e2985a4b
rajsharmabkbiet25-stack <raj.sharmabkbiet25@gmail.com>
2026-06-26 07:42:57 +0530

    use diff approch for security group

--- main.tf
+++ main.tf
@@ -176,7 +176,8 @@ module "blog_asg" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   launch_template_name = "blog"
-  vpc_security_group_ids = [module.blog_sg.id]
+  // vpc_security_group_ids = [module.blog_sg.id]
+  security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
